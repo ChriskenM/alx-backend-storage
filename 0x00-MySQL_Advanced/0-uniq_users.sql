dbbd452e4c3e62a723cac56e0ace2fc048667d04
ChriskenM <chriskenmutuma0727@gmail.com>
2024-05-09 06:34:09 +0000

    testing database creation

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Script to creates table users if it doesn't exist
 -- email attribute is unique
-CREATE TABLE IF NOT EXISTS holberton.users (
+CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
